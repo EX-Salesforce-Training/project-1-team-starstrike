173929b82b241e7635068efb8683924cb29f0be2
janhnhi <janhnhi@gmail.com>
2020-08-03 12:08:42 -0700

    Fixed file locations

---
+++ .sfdx/typings/lwc/sobjects/Address__ChangeEvent.d.ts
@@ -0,0 +1,72 @@
+declare module "@salesforce/schema/Address__ChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Address_Line_1__c" {
+  const Address_Line_1__c:string;
+  export default Address_Line_1__c;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Address_Line_2__c" {
+  const Address_Line_2__c:string;
+  export default Address_Line_2__c;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.City__c" {
+  const City__c:string;
+  export default City__c;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Postal_Zip_Code__c" {
+  const Postal_Zip_Code__c:string;
+  export default Postal_Zip_Code__c;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.State__c" {
+  const State__c:string;
+  export default State__c;
+}
+declare module "@salesforce/schema/Address__ChangeEvent.Country__c" {
+  const Country__c:string;
+  export default Country__c;
+}
---
+++ .sfdx/typings/lwc/sobjects/Contact_Address__ChangeEvent.d.ts
@@ -0,0 +1,48 @@
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.ChangeEventHeader" {
+  const ChangeEventHeader:any;
+  export default ChangeEventHeader;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.Contact__c" {
+  const Contact__c:any;
+  export default Contact__c;
+}
+declare module "@salesforce/schema/Contact_Address__ChangeEvent.Address__c" {
+  const Address__c:any;
+  export default Address__c;
+}
